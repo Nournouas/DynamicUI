9c32f1ff3864d1c9af7d875870fa64def37b983e
Nournouas <171356831+Nournouas@users.noreply.github.com>
2025-07-08 15:37:47 +0100

    added support for arrows using closures

--- src/index.js
+++ src/index.js
@@ -24,29 +24,39 @@ function Carousel () {
     const moveDistance = moveStart();
     rightButton.addEventListener("click", () => moveRightFunctionality(moveDistance, carousel));
     leftButton.addEventListener("click", () => moveLeftFunctionality(moveDistance, carousel));
+    /*setInterval(()=>{
+        moveRightFunctionality(moveDistance, carousel)
+    }, 50);*/
 }
 
 function moveStart () {
-    let move = 0;
+    let currentIndex = 0;
+    const carouselDiv = document.querySelector(".carousel-div");
+    const widthOfCard = carouselDiv?.clientWidth;
+
+    const careouselMain = document.querySelector(".carousel-main");
+    const numberOfSlides =  careouselMain.childElementCount;
+    console.log(-(widthOfCard * numberOfSlides));
 
     return function moveInner(direction) {
+
         if (direction === "left"){
-            move += 60;
-            if (move > 0){
-                move = move * -3
-                return move
+            currentIndex += widthOfCard;
+            if (currentIndex > 0){
+                currentIndex = currentIndex * (-(numberOfSlides - 1));
+                return(currentIndex);
             } else{
-                return move;
+                return currentIndex;
             }
         }
 
         if (direction === "right"){
-            move -= 60;
-            if (move < -180){
-                move = 0
-                return move
+            currentIndex -= widthOfCard;
+            if (currentIndex <= -(widthOfCard * numberOfSlides)){
+                currentIndex = 0
+                return currentIndex
             } else{
-                return move;
+                return currentIndex;
             }
         }
     };
@@ -57,16 +67,15 @@ function moveStart () {
 function moveRightFunctionality(moveDistanceRight, carousel){
         let rightDistance = moveDistanceRight("right");
         console.log(rightDistance);
-        carousel.style = "";
-        carousel.style.transform += `translateX(${rightDistance}vw)`
+        carousel.style.transform = `translateX(${rightDistance}px)`
 }
 
 function moveLeftFunctionality(moveDistanceLeft, carousel){
         let leftDistance = moveDistanceLeft("left");
         console.log(leftDistance);
-        carousel.style = "";
-        carousel.style.transform += `translateX(${leftDistance}vw)`
+        carousel.style.transform = `translateX(${leftDistance}px)`
 }
 
 Carousel();
 navButtonHover();
+
